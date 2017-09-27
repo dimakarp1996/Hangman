0609b0dfb5159e0b9499f3c92f46d31668bf14c9
dimakarp1996 <dimakarp1996@yandex.ru>
2017-09-27 14:57:15 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,35 @@
+
+"""Setup script."""
+
+from setuptools import setup
+
+setup(
+    name="Hangman",
+    version="0.0.0",
+    author="Dmitry Karpov",
+    author_email="dimakarp1996@yandex.ru",
+    url="https://github.com/dimakarp1996/Hangman",
+    license="MIT",
+    packages=['Hangman'],
+    install_requires=[
+    ],
+    setup_requires=[
+        "pytest-runner",
+        "pytest-pylint",
+        "pytest-pycodestyle",
+        "pytest-pep257",
+        "pytest-cov",
+    ],
+    tests_require=[
+        "pytest",
+        "pylint",
+        "pycodestyle",
+        "pep257",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ]
+)
