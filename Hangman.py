9622ce5b71852208a0fa0385d2e81df11ec337c3
dimakarp1996 <dimakarp1996@yandex.ru>
2017-10-01 14:27:04 +0300

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -5,29 +5,32 @@ def loss(attempts, num):#calculate loss
     if attempts >= num:
         return 'You lost!'
     return 'You won!'
-def play():
-    random.seed(0)
-    i = random.randint(0, 4)
-    DICT = ['hello', 'apple', 'orange', 'smell', 'brink']
-    print("GUESS a LETTER:")
-    LETTER = DICT[i]
-    N = len(LETTER)
-    GUESS = '*'*N
-    typed=[]
-    ATTEMPTS = 0
-    while ATTEMPTS < N:
-        INP = input()
-        INP = INP[0]
-        if INP in LETTER:
-            for j in range(N):
-                if LETTER[j] == INP and LETTER[j] not in typed:
-                    GUESS = GUESS[:j]+INP+GUESS[j+1:]
-            print('Hit!')
-            print('The word: '+GUESS)
-            if GUESS == LETTER:
-                break
-        else:
-            ATTEMPTS += 1
-            print('Nussed, mistake %d out of %d'% (ATTEMPTS, N))
-    print(loss(ATTEMPTS, N)+'\n')
-play()
+def check(INP,GUESS,LETTER,ATTEMPTS):
+    INP=INP[0]
+    if INP in LETTER:
+        for j in range(N):
+            if LETTER[j] == INP:
+                GUESS = GUESS[:j]+INP+GUESS[j+1:]
+        print('Hit!')
+        print('The word: '+GUESS)
+    else:
+        ATTEMPTS += 1
+        print('Nussed, mistake %d out of %d'% (ATTEMPTS, N))
+    return GUESS,LETTER,ATTEMPTS
+random.seed(0)
+i = random.randint(0, 4)
+DICT = ['hello', 'apple', 'orange', 'smell', 'brink']
+print("GUESS a LETTER:")
+LETTER = DICT[i]
+N = len(LETTER)
+GUESS = '*'*N
+REQUESTS = []
+ATTEMPTS = 0
+while ATTEMPTS < N:
+    INP = input()
+    REQUESTS.append(INP[0])
+    INP = INP[0]
+    GUESS,LETTER,ATTEMPTS=check(INP,GUESS,LETTER,ATTEMPTS)
+    if GUESS == LETTER:
+            break
+print(loss(ATTEMPTS, N)+'\n')
