b2ad39602e29ff8ba3444347f14d3e897c306936
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 13:41:42 +0300

    Add files via upload

--- tests/test_check.py
+++ tests/test_check.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 14 11:52:21 2017
-
 @author: tcs-user
 """
 from itertools import combinations
@@ -14,12 +13,12 @@ def test_check():
     for word in dict:
         for i in range(4):
             current = word
-            letters=combinations(word,i)
-                for letter in letters:
-                    current = current.replace(letter, '*')
+            letters = combinations(word, i)
+            for letter in letters:
+                current = current.replace(letter, '*')
             if current not in game_situations:
                 game_situations.append(current)
-               
+
     alph = 'abcdefghijklmnopqrstuvwxyz'
     for i in range(4):
         for word in DICT:
@@ -27,9 +26,9 @@ def test_check():
                 for guess in game_situations:
                     if lett not in word:
                         assert check(lett, guess, word,
-                                 i) == (guess, word, i + 1)
+                                     i) == (guess, word, i + 1)
                     else:
-                        ind=word.find(lett)
-                        guess1=guess[:ind] + lett + guess[ind:]
+                        ind = word.find(lett)
+                        guess1 = guess[:ind] + lett + guess[ind:]
                         assert check(lett, guess, word,
-                                 i) == (guess1, word, i)
+                                     i) == (guess1, word, i)
