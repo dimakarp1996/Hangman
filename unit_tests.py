e41786e9b0be4ab51b0a57582520d38741dee916
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 11:59:33 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -7,7 +7,11 @@ Created on Tue Nov 14 11:00:45 2017
 """
 
 import mock
-import test_check, test_loss, test_gener_random, test_get_input, test_gener_init
+import test_check
+import test_loss
+import test_gener_random
+import test_get_input
+import test_gener_init
 
 test_check()
 test_loss()
