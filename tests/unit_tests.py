92219406abb863048607ab5fa80006efc68b4a32
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 18:46:16 +0300

    Create unit_tests.py

---
+++ tests/unit_tests.py
@@ -0,0 +1,14 @@
+from test_check import test_check
+from test_loss import test_loss
+from test_gener_random import test_gener_random
+from test_get_input import test_get_input
+from test_gener_init import test_gener_init
+from test_play import test_play
+
+test_check()
+test_loss()
+test_gener_random()
+test_get_input()
+test_gener_init()
+test_play()
+print('All tests passed correctly')
