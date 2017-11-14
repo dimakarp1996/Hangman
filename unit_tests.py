e3a614a93c92a6ca56e4e3eac0f03ae0d38e3919
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 18:26:55 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,14 +1,12 @@
-from test_check import test_check
-from test_loss import test_loss
-from test_gener_random import test_gener_random
-from test_get_input import test_get_input
-from test_gener_init import test_gener_init
-from test_play import test_play
+from tests import test_check
+from tests import test_loss
+from tests import test_gener_random
+from tests import test_get_input
+from tests import test_gener_init
 
-test_check()
-test_loss()
-test_gener_random()
-test_get_input()
-test_gener_init()
-test_play()
+test_check.test_check()
+test_loss.test_loss()
+test_gener_random.test_gener_random()
+test_get_input.test_get_input()
+test_gener_init.test_gener_init()
 print('All tests passed correctly')
