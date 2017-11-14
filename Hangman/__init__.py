753d2bfaf1aa04626e7997404352c44f7c186787
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 13:12:27 +0300

    Update __init__.py

--- Hangman/__init__.py
+++ Hangman/__init__.py
@@ -7,7 +7,7 @@ Created on Tue Nov 14 10:59:51 2017
 """
 
 import random  # calculate random numbers
-DICT = ['hello', 'apple', 'phone', 'smell', 'brink']
+DICT = ['east', 'eats' ,'seat', 'teas']
 
 
 def loss(attempts, num):  # calculate loss
@@ -38,7 +38,7 @@ def get_input():
 
 def generate_random():
     random.seed(0)
-    i = random.randint(0, 4)
+    i = random.randint(0, 3)
     print("GUESS a LETTER:")
     return i
 
