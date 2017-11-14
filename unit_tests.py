a8c6e13ba5036a0a067782132c1f076d0f0c7f4a
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 18:06:53 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 14 11:00:45 2017
-
-@author: tcs-user
-"""
-
-import mock
-from tests import test_check
-from tests import test_loss
-from tests import test_gener_random
-from tests import test_get_input
-from tests import test_gener_init
+from test_check import test_check
+from test_loss import test_loss
+from test_gener_random import test_gener_random
+from test_get_input import test_get_input
+from test_gener_init import test_gener_init
 
 
 test_check.test_check()
