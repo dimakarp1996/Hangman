f18670f48af99ccc13e6305e09dac0c9bb58c969
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 12:20:50 +0300

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,11 +13,11 @@ Created on Tue Nov 14 11:00:45 2017
 """
 
 import mock
-import test_check
-import test_loss
-import test_gener_random
-import test_get_input
-import test_gener_init
+from test_check import test_check
+from test_loss import test_loss
+from test_gener_random import test_gener_random
+from test_get_input import test_get_input
+from test_gener_init import test_gener_init
 
 test_check()
 test_loss()
